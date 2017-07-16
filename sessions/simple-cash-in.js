76b91c66ed3eb26b82678d6af3df82923faed84f
Josh Harvey <josh@lamassu.is>
2017-07-16 13:24:09 +0300

    WIP

--- sessions/simple-cash-in.js
+++ sessions/simple-cash-in.js
@@ -1,27 +1,35 @@
 const StateMachine = require('javascript-state-machine')
 
-const fsm = new StateMachine({
-  init: 'start',
+const SimpleCashIn = StateMachine.factory({
+  init: 'init',
   transitions: [
-    {name: 'chooseCoin', from: 'start', to: 'readyForBill'},
-    {name: 'scanned', from: 'readyForBill', to: 'processingBill'},
+    {name: 'wsChooseCoin', from: 'init', to: 'scanning'},
+    {name: 'wsScanned', from: 'scanning', to: 'readyForBill'},
+    {name: 'insertBill', from: 'readyForBill', to: 'processingBill'},
     {name: 'processBill', from: 'processingBill', to: 'readyForBill'},
     {name: 'sendCoins', from: 'readyForBill', to: 'waitingForReceipt'},
-    {name: 'cryptoTransferPending', from: 'waitingForReceipt', to: 'waitingForCompleted'},
-    {name: 'complete', from: 'waitingForCompleted', to: 'start'}
+    {name: 'wsCryptoTransferPending', from: 'waitingForReceipt', to: 'waitingForCompleted'},
+    {name: 'complete', from: 'waitingForCompleted', to: 'completed'}
   ],
+  data: bills => ({bills, iteration: 0}),
   methods: {
-    onReadyForBill,
-    onSendCoins,
-    onWaitingForCompleted
+    onReadyForBill
   }
 })
 
+module.exports = SimpleCashIn
+
 function onReadyForBill () {
-}
+  const bill = this.bills[this.iteration]
 
-function onSendCoins () {
-}
+  console.log('DEBUG100: %j', {bill, bills: this.bills})
 
-function onWaitingForCompleted () {
+  if (!bill) {
+    this.iteration = 0
+    this.sendCoins()
+    return
+  }
+
+  this.iteration = this.iteration + 1
+  this.insertBill(bill)
 }
