dd3d3cfb93e5e89399f3d88c1f3df6982b4afca7
Josh Harvey <josh@lamassu.is>
2017-07-18 08:12:16 +0300

    WIP

--- sessions/simple-cash-in.js
+++ sessions/simple-cash-in.js
@@ -1,16 +1,19 @@
-const StateMachine = require('javascript-state-machine')
 
-const SimpleCashIn = StateMachine.factory({
-  init: 'init',
-  transitions: [
-    {name: 'wsChooseCoin', from: 'init', to: 'scanning'},
-    {name: 'wsScanned', from: 'scanning', to: 'readyForBill'},
-    {name: 'insertBill', from: 'readyForBill', to: 'processingBill'},
-    {name: 'processBill', from: 'processingBill', to: 'readyForBill'},
-    {name: 'sendCoins', from: 'readyForBill', to: 'waitingForReceipt'},
-    {name: 'wsCryptoTransferPending', from: 'waitingForReceipt', to: 'waitingForCompleted'},
-    {name: 'complete', from: 'waitingForCompleted', to: 'completed'}
-  ]
+const machina = require('machina')
+
+const SimpleCashIn = new machina.BehavioralFsm({
+  initialState: 'start',
+  states: {
+    start: {wsChooseCoin: 'scanning'},
+    scanning: {wsScanned: 'readyForBill'},
+    readyForBill: {
+      insertBill: 'processingBill',
+      sendCoins: 'waitingForReceipt'
+    },
+    processingBill: {processBill: 'readyForBill'},
+    waitingForReceipt: {wsCryptoTransferPending: 'waitingForCompleted'},
+    waitingForCompleted: {complete: 'completed'}
+  }
 })
 
 module.exports = SimpleCashIn
